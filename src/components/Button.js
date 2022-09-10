900a9538791fd1ccb9cc72fc40ca656e626c2bf1
Brenda <brendamvreal@gmail.com>
2022-09-10 12:10:50 -0400

    Refactor component to apply condition CSS based on the value.

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,15 +3,23 @@ import React from "react";
 import "components/Button.scss";
 
 export default function Button(props) {
-   let buttonClass = "button";
+  let buttonClass = "button";
+  
+  if (props.confirm) {
+    buttonClass += " button--confirm";
+  }
 
-   if (props.confirm) {
-      buttonClass += " button--confirm"
-   }
+  if (props.danger) {
+    buttonClass += " button--danger";
+  }
 
-   if (props.danger) {
-      buttonClass += " button--danger"
-   }
-
-   return <button className={buttonClass}>{props.children}</button>
+  return (
+    <button
+      className={buttonClass}
+      onClick={props.onClick}
+      disabled={props.disabled}
+    >
+      {props.children}
+    </button>
+  );
 }
