666d34f64daef09d85fd8de007899a431453f8f5
Brenda <brendamvreal@gmail.com>
2022-09-11 23:38:28 -0400

    Create dinamic styles based on DayListItem component.

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,12 +1,21 @@
 import React from "react";
+import classNames from "classnames";
 
-export default function DayListItem(props) { // component takes 3 tributes, names - spots - selected
+import "components/DayListItem.scss";
+
+export default function DayListItem(props) {
+  // component takes 3 tributes, names - spots - selected
+  const dayClass = classNames({
+    "day-list__item": true,
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots === 0,
+  });
+
   return (
     // <li> is the entire day <h2> display the day name <h3> spots remaining
     <li onClick={() => props.setDay(props.name)}>
-      <h2 className="text--regular">{props.name}</h2> 
-      <h3 className="text--light">{props.spots}</h3>
+      <h2 className={dayClass}>{props.name}</h2>
+      <h3 className={dayClass}>{props.spots} spots remaining</h3>
     </li>
   );
 }
-
