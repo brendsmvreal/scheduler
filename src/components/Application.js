fb27d995f5f951fc512c069a6bc50d69f25d739b
Brenda <brendamvreal@gmail.com>
2022-09-13 01:17:08 -0400

    Update the name of props passed to DayList to improve redability.

--- src/components/Application.js
+++ src/components/Application.js
@@ -38,9 +38,8 @@ export default function Application(props) {
         <nav className="sidebar__menu">
           <DayList
             days={days}
-            day={day}
-            setDay={setDay}
-            // onChange={(day) => console.log(day)}
+            value={day}
+            onChange={setDay}
           />
         </nav>
         <img
