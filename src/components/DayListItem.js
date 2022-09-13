f1f34ffd67955409109d35016e82bfba972f85ac
Brenda <brendamvreal@gmail.com>
2022-09-13 01:13:53 -0400

    Format file.

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -3,30 +3,31 @@ import classNames from "classnames";
 
 import "./DayListItem.scss";
 
-
 export default function DayListItem(props) {
   // component takes 3 tributes, names - spots - selected
-  const dayClass = classNames(
-    "day-list__item", {
+  const dayClass = classNames("day-list__item", {
     "day-list__item--selected": props.selected,
     "day-list__item--full": props.spots === 0,
   });
-  
-  const formatSpots = function(spots) {
-    
+
+  const formatSpots = function (spots) {
     if (spots === 0) {
-      return 'no spots remaining';
+      return "no spots remaining";
     }
     if (spots === 1) {
-      return (spots + ' spot remaining');
+      return spots + " spot remaining";
     } else {
-      return (spots + ' spots remaining');
+      return spots + " spots remaining";
     }
   };
-  
+
   return (
     // <li> is the entire day <h2> display the day name <h3> spots remaining
-    <li className={dayClass} onClick={() => props.setDay(props.name)} selected={props.selected}>
+    <li
+      className={dayClass}
+      onClick={() => props.setDay(props.name)}
+      selected={props.selected}
+    >
       <h2 className="text--regular">{props.name}</h2>
       <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
