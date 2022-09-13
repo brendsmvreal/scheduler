27a4aa569a2898eedd0da22e51d04a07e677db54
Brenda <brendamvreal@gmail.com>
2022-09-13 01:02:16 -0400

    Add selected props to render to DayList component.

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -26,7 +26,7 @@ export default function DayListItem(props) {
   
   return (
     // <li> is the entire day <h2> display the day name <h3> spots remaining
-    <li className={dayClass} onClick={() => props.setDay(props.name)}>
+    <li className={dayClass} onClick={() => props.setDay(props.name)} selected={props.selected}>
       <h2 className="text--regular">{props.name}</h2>
       <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
