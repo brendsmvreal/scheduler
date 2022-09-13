d9a7def9ea893bdc78f0d663b8f78769c15dd50a
Brenda <brendamvreal@gmail.com>
2022-09-13 00:41:18 -0400

    Refactor InterviewList and InterviewerListItem to avoid passing the id prop to the ListItem.

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -14,7 +14,7 @@ export default function InterviewerList(props) {
         name={interviewer.name}
         avatar={interviewer.avatar}
         selected={interviewer.id === props.interviewer} //props.interviewer = id
-        setInterviewer={props.setInterviewer}
+        setInterviewer={() => props.setInterviewer(interviewer.id)}
       />
     );
   });
