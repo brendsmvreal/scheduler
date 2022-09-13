0a9a797fc6b323922c6595a776262157a695cee4
Brenda <brendamvreal@gmail.com>
2022-09-13 00:26:44 -0400

    Create InterviewerList component.

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,27 @@
+import React from "react";
+import classNames from "classnames";
+import "./InterviewerList.scss";
+import InterviewerListItem from "./InterviewerListItem";
+
+export default function InterviewerList(props) {
+  const interviewerListContainer = props.interviewers.map((interviewer) => {
+  // console.log("props.interviewer-----", props.interviewer, "interviewer.id-----", interviewer.id)
+
+    return (
+      <InterviewerListItem
+        key={interviewer.id}
+        id={interviewer.id}
+        name={interviewer.name}
+        avatar={interviewer.avatar}
+        selected={interviewer.id === props.interviewer} //props.interviewer = id
+        setInterviewer={props.setInterviewer}
+      />
+    );
+  });
+  return (
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list">{interviewerListContainer}</ul>
+    </section>
+  );
+}
