de84715a031cbbd2af310efaf7f2e25c4295827f
Brenda <brendamvreal@gmail.com>
2022-09-15 00:03:24 -0400

    Implement Appointment component to condionally render its childs components.

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,7 +1,27 @@
 import React from "react";
 
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
+
 import "./styles.scss";
 
-export default function Application(props) {
-  return <article className="appointment"></article>;
+export default function Appointment(props) {
+  // props.interview ?
+  console.log("props.student------", props.student);
+  return (
+    <article className="appointment">
+      <header>
+        <Header time={props.time} />
+        {props.interview ? (
+          <Show
+            student={"Lydia Miller-Jones"}
+            interviewer={props.interview.interviewer.name}
+          />
+        ) : (
+          <Empty />
+        )}
+      </header>
+    </article>
+  );
 }
