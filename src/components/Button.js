7f8a645ce9278e239d225172041ed339c5aa5f2a
Brenda <brendamvreal@gmail.com>
2022-09-14 23:06:05 -0400

    Add comments.

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,14 +3,14 @@ import classNames from "classnames";
 
 import "./Button.scss";
 
-export default function Button(props) {
+export default function Button(props) { // Button is the parent component 
   const buttonClass = classNames("button", {
     "button--confirm": props.confirm,
     "button--danger": props.danger,
   });
 
   return (
-    <button
+    <button // child HTML component of Button
       className={buttonClass}
       onClick={props.onClick}
       disabled={props.disabled}
