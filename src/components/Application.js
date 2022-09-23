6a86eae94da417e79a03db42f397686dbb7f3fbf
Brenda <brendamvreal@gmail.com>
2022-09-23 00:09:10 -0400

    Add useEffect with API request.

--- src/components/Application.js
+++ src/components/Application.js
@@ -2,24 +2,8 @@ import React, { useState } from "react";
 import "./Application.scss";
 import Appointment from "./Appointment";
 import DayList from "./DayList";
-
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
+import axios from "axios";
+import { useEffect } from "react";
 
 const appointments = {
   1: {
@@ -62,8 +46,10 @@ const appointments = {
 
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
+  const [days, setDays] = useState([]);
 
-  const arrAppts = Object.values(appointments).map((appointment) => { // props could be represented by ... spread: key={appointment.id}{... appointment}
+  const arrAppts = Object.values(appointments).map((appointment) => {
+    // props could be represented by ... spread: key={appointment.id}{... appointment}
     return (
       <Appointment
         key={appointment.id}
@@ -73,6 +59,13 @@ export default function Application(props) {
       />
     );
   });
+
+  useEffect(() => {
+    axios.get("http://localhost:8001/api/days").then((response) => {
+      console.log(response.data);
+      setDays(response.data);
+    });
+  }, []);
 
   return (
     <main className="layout">
@@ -93,8 +86,8 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-      <Appointment key="last" time="5pm" />
-      {arrAppts}
+        <Appointment key="last" time="5pm" />
+        {arrAppts}
       </section>
     </main>
   );
