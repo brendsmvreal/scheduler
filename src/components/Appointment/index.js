11d2359f2d265a242ee1d06a92d072648002a677
Brenda <brendamvreal@gmail.com>
2022-09-23 01:24:10 -0400

    Pass interviewer name to <Show tag.

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -15,7 +15,7 @@ export default function Appointment(props) {
         <Header time={props.time} />
         {props.interview ? (
           <Show
-            student={"Lydia Miller-Jones"}
+            student={props.interview.student}
             interviewer={props.interview.interviewer.name}
           />
         ) : (
