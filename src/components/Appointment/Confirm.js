25a9c2d2cf3f2e0ec055b5e16902278d2612bc57
Brenda <brendamvreal@gmail.com>
2022-10-02 01:21:21 -0400

    Fix spacing.

--- src/components/Appointment/Confirm.js
+++ src/components/Appointment/Confirm.js
@@ -1,18 +1,20 @@
 import React from "react";
-
 import Button from "components/Button";
 
 export default function Confirm(props) {
   return (
     <main className="appointment__card appointment__card--confirm">
-      <h1 className="text--semi-bold">Delete the appointment?</h1>
+      <h1 className="text--semi-bold">{props.message}</h1>
       <section className="appointment__actions">
-        <Button danger onClick={props.onCancel}>Cancel</Button>
-        <Button confirm onClick={props.onConfirm}>Confirm</Button>
+        <Button onClick={props.onCancel} danger>
+          Cancel
+        </Button>
+        <Button onClick={props.onConfirm} danger>
+          Confirm
+        </Button>
       </section>
     </main>
   );
 }
-
 /* onConfirm={action("onConfirm")}
 onCancel={action("onCancel")} */
