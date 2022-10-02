30985551bd2c8bcedbdbb5037ee16c86805d7b3d
Brenda <brendamvreal@gmail.com>
2022-10-02 02:29:29 -0400

    Pass property interviewer to Form component.

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -99,6 +99,7 @@ export default function Appointment(props) {
         {mode === EDIT && (
           <Form
             student={props.interview.student}
+            interviewer={props.interview.interviewer.id}
             interviewers={props.interviewers}
             onSave={save}
             onCancel={() => back()}
