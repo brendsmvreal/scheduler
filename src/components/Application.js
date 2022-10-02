d2000589daae5b67d793990767931403832b5eee
Brenda <brendamvreal@gmail.com>
2022-10-02 01:16:41 -0400

    Create function to cancel interviews.

--- src/components/Application.js
+++ src/components/Application.js
@@ -68,6 +68,25 @@ export default function Application(props) {
       });
   };
 
+  const cancelInterview = function (id) {
+    const appointment = {
+      ...state.appointments[id],
+      interview: null,
+    };
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment,
+    };
+    return axios
+      .delete(`http://localhost:8001/api/appointments/${id}`)
+      .then(() => {
+        setState({
+          ...state,
+          appointments,
+        });
+      });
+  };
+
   // gets intereviewers for certain day
   const interviewers = getInterviewersForDay(state, state.day);
   // gets appointments for certain day
@@ -80,6 +99,7 @@ export default function Application(props) {
         interview={getInterview(state, appointment.interview)}
         interviewers={interviewers}
         bookInterview={bookInterview}
+        cancelInterview={cancelInterview}
       />
     );
   });
