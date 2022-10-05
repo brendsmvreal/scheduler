36c8e06330e737dcd5cd270588ce048318de0913
Brenda <brendamvreal@gmail.com>
2022-10-04 23:30:23 -0400

    Add error handling and mock data for testing.

--- src/components/__tests__/ Appointment.test.js
+++ src/components/__tests__/ Appointment.test.js
@@ -1,11 +1,28 @@
 import React from "react";
 import { render, cleanup } from "@testing-library/react";
 import Appointment from "components/Appointment";
+import Application from "components/Application";
+import { waitForElement, getByText } from "@testing-library/react";
 
 afterEach(cleanup);
 
 describe("Appointment", () => {
-  it("renders without crashing", () => {
+  it("defaults to Monday and changes the schedule when a new day is selected", () => {
     render(<Appointment />);
+  });
+
+  it("defaults to Monday and changes the schedule when a new day is selected", () => {
+    const { getByText } = render(<Application />);
+
+    return waitForElement(() => getByText("Monday"));
+  });
+
+  it("defaults to Monday and changes the schedule when a new day is selected", () => {
+    const { getByText } = render(<Application />);
+
+    return waitForElement(() => getByText("Monday")).then(() => {
+      fireEvent.click(getByText("Tuesday"));
+      expect(getByText("Leopold Silvers")).toBeInTheDocument();
+    });
   });
 });
