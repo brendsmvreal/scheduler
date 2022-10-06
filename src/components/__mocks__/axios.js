3af5fe4b1e61d4e040d104c29a5a533c7ddd9d79
Brenda <brendamvreal@gmail.com>
2022-10-06 00:25:04 -0400

    Add in mock data for testing.

--- src/components/__mocks__/axios.js
+++ src/components/__mocks__/axios.js
@@ -54,9 +54,9 @@ const fixtures = {
 };
 
 export default {
-  defaults: { baseURL: "" },
+  defaults: { baseURL: "http://localhost:8001" },
   get: jest.fn((url) => {
-    if (url === "/api/days/") {
+    if (url === "/api/days") {
       return Promise.resolve({
         status: 200,
         statusText: "OK",
@@ -64,7 +64,7 @@ export default {
       });
     }
 
-    if (url === "/api/appointments/") {
+    if (url === "/api/appointments") {
       /* Resolve appointments data */
       return Promise.resolve({
         status: 200,
@@ -73,7 +73,7 @@ export default {
       });
     }
 
-    if (url === "/api/interviewers/") {
+    if (url === "/api/interviewers") {
       /* Resolve interviewers data */
       return Promise.resolve({
         status: 200,
@@ -81,17 +81,5 @@ export default {
         data: fixtures.interviewers,
       });
     }
-  }),
-  put: jest.fn((url) => {
-    return Promise.resolve({
-      status: 204,
-      statusText: "No Content",
-    });
-  }),
-  delete: jest.fn((url) => {
-    return Promise.resolve({
-      status: 204,
-      statusText: "No Content",
-    });
   }),
 };
