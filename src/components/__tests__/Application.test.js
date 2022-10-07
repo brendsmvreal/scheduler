236850acda54b5a81372ab011987be788b7ab88c
Brenda <brendamvreal@gmail.com>
2022-10-07 15:23:35 -0400

    Write more tests other modes.

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -32,7 +32,7 @@ describe("Appointment", () => {
   });
   // ---- Test 2 ----
   it("loads data, books an interview and reduces the spots remaining for Monday by 1", async () => {
-    const { container } = render(<Application />);
+    const { container, debug } = render(<Application />);
 
     await waitForElement(() => getByText(container, "Archie Cohen"));
 
@@ -44,10 +44,18 @@ describe("Appointment", () => {
     fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
       target: { value: "Lydia Miller-Jones" },
     });
-    fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
 
+    fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
     fireEvent.click(getByText(appointment, "Save"));
 
-    // console.log(prettyDOM(appointment));
+    expect(getByText(appointment, "Saving")).toBeInTheDocument();
+
+    await waitForElement(() => getByText(appointment, "Lydia Miller-Jones"));
+
+    const day = getAllByTestId(container, "day").find((day) =>
+      queryByText(day, "Monday")
+    );
+
+    expect(getByText(day, "no spots remaining")).toBeInTheDocument();
   });
 });
