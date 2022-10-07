641da92f79081b87203c45f823b4f3585398500d
Brenda <brendamvreal@gmail.com>
2022-10-07 15:19:34 -0400

    Import queries gtom react library and trigger test with three click events.

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,25 +1,53 @@
 import React from "react";
+import axios from "axios";
+
 import {
   render,
   cleanup,
   waitForElement,
   fireEvent,
-  getByText,
   prettyDOM,
+  getByText,
   getAllByTestId,
   getByAltText,
   getByPlaceholderText,
-  getAllByText,
+  queryByText,
+  queryByAltText,
+  waitForElementToBeRemoved,
 } from "@testing-library/react";
+
 import Application from "components/Application";
 
 afterEach(cleanup);
 
-it("defaults to Monday and changes the schedule when a new day is selected", () => {
-  const { getByText } = render(<Application />);
+describe("Appointment", () => {
+  // ---- Test 1 ----
+  it("defaults to Monday and changes the schedule when a new day is selected", () => {
+    const { getByText } = render(<Application />);
 
-  return waitForElement(() => getByText("Monday")).then(() => {
-    fireEvent.click(getByText("Tuesday"));
-    expect(getByText("Leopold Silvers")).toBeInTheDocument();
+    return waitForElement(() => getByText("Monday")).then(() => {
+      fireEvent.click(getByText("Tuesday"));
+      expect(getByText("Leopold Silvers")).toBeInTheDocument();
+    });
+  });
+  // ---- Test 2 ----
+  it("loads data, books an interview and reduces the spots remaining for Monday by 1", async () => {
+    const { container } = render(<Application />);
+
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+
+    const appointments = getAllByTestId(container, "appointment");
+    const appointment = appointments[0];
+
+    fireEvent.click(getByAltText(appointment, "Add"));
+
+    fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+      target: { value: "Lydia Miller-Jones" },
+    });
+    fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+
+    fireEvent.click(getByText(appointment, "Save"));
+
+    // console.log(prettyDOM(appointment));
   });
 });
